95e5b21ea9a357bdde77919d936e200e8d2c7439
Yijun Xiao <yijunxiao@cs.ucsb.edu>
2018-05-14 08:42:46 -0700

    modified launch script generator

--- get_launch_script.py
+++ get_launch_script.py
@@ -8,19 +8,27 @@ parser.add_argument('--num_topics', type=int, default=8,
                     help="number of topics to use for the training")
 parser.add_argument('--joint', action='store_true',
                     help="use joint model, otherwise use marginal model")
+parser.add_argument('--bow', action='store_true',
+                    help="add bow loss during training")
 args = parser.parse_args()
 
 
-command_base = 'python main.py --data={0} --num_topics={1:d} --wd={2:.0e} --kla{3}'
+command_base = 'python main.py --data={0} --num_topics={1:d} --wd={2:.0e} --kla{3}{4}'
 dataset = args.data.rstrip('/').split('/')[-1]
-logpath_base = 'logs/{0}/z32.tpc{1:d}.wd{2:.0e}.kla{3}.{0}.log'
+logpath_base = 'logs/{0}/z32.tpc{1:d}.wd{2:.0e}.kla{3}{4}.{0}.log'
 lines = []
 for wd in [0.001, 0.003, 0.01]:
-    command = command_base.format(args.data, args.num_topics, wd, ' --joint' if args.joint else '')
-    logpath = logpath_base.format(dataset, args.num_topics, wd, '.jt' if args.joint else '')
+    command = command_base.format(args.data, args.num_topics, wd,
+                                  ' --bow' if args.bow else '',
+                                  ' --joint' if args.joint else '')
+    logpath = logpath_base.format(dataset, args.num_topics, wd,
+                                  '.bow' if args.bow else '',
+                                  '.jt' if args.joint else '')
     lines.append(command + ' > ' + logpath)
 
-script_name = '{0}.tpc{1:d}{2}.sh'.format(dataset, args.num_topics, '.jt' if args.joint else '')    
+script_name = '{0}.tpc{1:d}{2}{3}.sh'.format(dataset, args.num_topics,
+                                             '.bow' if args.bow else '',
+                                             '.jt' if args.joint else '')    
 with open(script_name, 'w') as f:
     f.write('\n'.join(lines))
     
